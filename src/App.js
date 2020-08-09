9c4b250553cb41a2c2e58d23dbced7cbc97a6d78
raman <ramanvellore2018@gmail.com>
2020-08-09 21:30:03 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Home from './Home'
+import About from './About'
+import Users from './Users'
+import { Navbar, Nav, } from 'react-bootstrap'
+import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
+          <Navbar.Brand><Link to="/home">RAMAN</Link></Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            
+          <Nav className="mr-auto">
+            <Nav.Link><Link to="/">Home</Link></Nav.Link>
+            <Nav.Link><Link to="/about">About</Link></Nav.Link>
+            <Nav.Link><Link to="/users">Users</Link></Nav.Link>
+          </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Switch>
+         
+          <Route path="/about" component={About} ></Route>
+          <Route path="/users" component={Users} ></Route>
+          <Route path="/" component={Home} ></Route>
+          <Route path="/home" component={Home} ></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Users.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Users(){
+    return(
+        <div>
+            <h1>User Componenet</h1>
+        </div>
+    )
+}
+export default Users;
